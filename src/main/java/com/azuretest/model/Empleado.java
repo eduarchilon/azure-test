511f4d32bc4410a60c39773fcfa8304cd4a071cb
eduarchilon <eduarchilon@outlook.es>
2023-08-20 21:41:25 -0300

    fix(deploy): fix deploy maven

--- src/main/java/com/azuretest/model/Empleado.java
+++ src/main/java/com/azuretest/model/Empleado.java
@@ -1,10 +1,8 @@
 package com.azuretest.model;
-
-
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
 @Entity
 @Table
